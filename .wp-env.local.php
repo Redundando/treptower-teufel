694ce5a81fb1b33a5c58e1e7ba71ae661e8f5cec
Arved <info@primedope.com>
2026-01-15 11:24:30 +0100

    Stop tracking .wp-secrets.php

---
+++ .wp-env.local.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'DB_NAME' => 'treptower-teufel',
+	'DB_USER' => 'root',
+	'DB_PASSWORD' => '',
+	'DB_HOST' => 'localhost',
+	'WP_HOME' => 'http://localhost/treptower-teufel',
+	'WP_SITEURL' => 'http://localhost/treptower-teufel',
+	'WP_ENVIRONMENT_TYPE' => 'local',
+];
