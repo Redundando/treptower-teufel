bc7d47c7485d8808e240b630b951e301afcd186e
Arved <info@primedope.com>
2026-01-15 11:36:53 +0100

    .

--- .wp-env.staging.php
+++ .wp-env.staging.php
@@ -3,7 +3,7 @@ return [
 	'DB_NAME' => 'teufel_staging',
 	'DB_USER' => 'teufel_staging',
 	'DB_HOST' => 'k8m0.your-database.de',
-	'WP_HOME' => 'http://staging.treptower-teufel.de/',
-	'WP_SITEURL' => 'http://staging.treptower-teufel.de/',
+	'WP_HOME' => 'https://staging.treptower-teufel.de/',
+	'WP_SITEURL' => 'https://staging.treptower-teufel.de/',
 	'WP_ENVIRONMENT_TYPE' => 'staging',
 ];
