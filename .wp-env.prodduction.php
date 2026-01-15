873703e4919deaeaf59227958848090d7e959a46
Arved <info@primedope.com>
2026-01-15 13:58:38 +0100

    .

---
+++ .wp-env.prodduction.php
@@ -0,0 +1,9 @@
+<?php
+return [
+	'DB_NAME' => 'teufel_prod',
+	'DB_USER' => 'teufel_prod',
+	'DB_HOST' => 'kbky.your-database.de',
+	'WP_HOME' => 'https://www.treptower-teufel.de/',
+	'WP_SITEURL' => 'https://www.treptower-teufel.de/',
+	'WP_ENVIRONMENT_TYPE' => 'production',
+];
