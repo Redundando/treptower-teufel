d6faec10ddd57e964b8f7e2a32a93ec2568ac9c9
Arved <info@primedope.com>
2026-01-20 10:55:09 +0100

    .

--- wp-content/cache/matomo/cache/tracker/matomocache_allSiteUrlsPerSite.php
+++ wp-content/cache/matomo/cache/tracker/matomocache_allSiteUrlsPerSite.php
@@ -1,1 +1,1 @@
-<?php return unserialize('a:2:{s:8:"lifetime";i:1768892645;s:4:"data";a:1:{i:1;a:1:{i:0;s:33:"http://localhost/treptower-teufel";}}}', ['allowed_classes' => false]);
+<?php return unserialize('a:2:{s:8:"lifetime";i:1768898507;s:4:"data";a:1:{i:1;a:1:{i:0;s:33:"http://localhost/treptower-teufel";}}}', ['allowed_classes' => false]);
